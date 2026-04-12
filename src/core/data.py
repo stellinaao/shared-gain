fixed oddity with tent basis and confirmed that everything still runs, now need to determine how to filter out some funky units (only spike in <5% of trials) - sa

src/core/data.py
@@ -25,13 +25,17 @@ shutup.please()
 
 # CONSTANTS
 session_pattern = re.compile(r"^\d{8}_\d{6}$")
-subject_ids = np.array([subj_id for subj_id in os.listdir(DATA_DIR)])
+subject_ids = np.sort(
+    [subj_id for subj_id in os.listdir(DATA_DIR) if not subj_id.startswith(".")]
+)
 session_ids = [
-    [
-        sess_id
-        for sess_id in os.listdir(DATA_DIR / subj_id)
-        if session_pattern.match(sess_id)
-    ]
+    np.sort(
+        [
+            sess_id
+            for sess_id in os.listdir(DATA_DIR / subj_id)
+            if session_pattern.match(sess_id)
+        ]
+    )
     for subj_id in subject_ids
 ]
 

src/sg/fitlvm_utils.py
@@ -148,7 +148,7 @@ def get_dataset_dm(
     tvs = OHE().fit_transform(trial_data[task_vars]).todense()
     # print(f"mozza: {trai}, feta: {tvs.shape}")
     # tents
-    from external.NDNT.utils.NDNutils import tent_basis_generate
+    from ndnt.utils.NDNutils import tent_basis_generate
 
     num_trials = len(trial_data)
     xs = np.linspace(0, num_trials - 1, num_tents)
@@ -1343,7 +1343,7 @@ def fit_model(
     # print("hullo")
     from torch.optim import AdamW
 
-    from external.NDNT.training import EarlyStopping, LBFGSTrainer, Trainer
+    from ndnt.training import EarlyStopping, LBFGSTrainer, Trainer
 
     model.prepare_regularization()
 